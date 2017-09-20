b5e5e669c4cc8df5b07dd10265780aa6f6abb2ab
lofidewanto <lofidewanto@gmail.com>
2017-09-20 23:20:04 +0200

    add Foo JS native as example

--- src/main/java/de/jakobjarosch/test/client/EntryPoint.java
+++ src/main/java/de/jakobjarosch/test/client/EntryPoint.java
@@ -18,8 +18,36 @@
  */
 package de.jakobjarosch.test.client;
 
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+
 public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
+
+    private Foo foo;
+
     @Override
     public void onModuleLoad() {
+        Button button = new Button("Calculate me!");
+        RootPanel rootPanel = RootPanel.get("buttonFoo");
+        rootPanel.add(button);
+
+        button.addClickHandler(clickEvent -> createFoo());
+    }
+
+    private void createFoo() {
+        if (foo == null) {
+            // For the first time 42 because of the default value in JS
+            foo = new Foo();
+
+            final int sum = foo.sum();
+            Window.alert(Integer.toString(sum));
+        } else {
+            // Second time and next time 20
+            foo.x = 10;
+            foo.y = 10;
+
+            final int sum = foo.sum();
+            Window.alert(Integer.toString(sum));
+        }
     }
 }
