5dc60878ed544fe7bd4747356178e0d2524276be
lofidewanto <lofidewanto@gmail.com>
2018-06-28 22:58:39 +0200

    update

--- src/main/java/de/jakobjarosch/test/client/MyApp.java
+++ src/main/java/de/jakobjarosch/test/client/MyApp.java
@@ -21,14 +21,19 @@ package de.jakobjarosch.test.client;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
+import java.util.logging.Logger;
+
 @JsType(namespace = JsPackage.GLOBAL, name = "MyApp")
 public class MyApp {
+
+    private static Logger logger = Logger.getLogger(MyApp.class.getName());
 
     public void click() {
         Window.alert("Hello, I'm GWT JsInterop!\nThanks for clicking.");
     }
 
     public String clickApple() {
+        logger.info("Click Apple...");
         Apple apple = new Apple();
         final int sum = apple.sum();
 
